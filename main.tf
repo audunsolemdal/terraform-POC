d9c761e49ee547ea229e3b8c33bc596cce2e206d
Audun Solemdal <auls@equinor.com>
2019-11-08 13:20:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ provider "azurerm" {
   version = "= 1.27"
 }
 
-terraform {
-  required_version ">= 0.11.7"
-}
-
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "northeurope"
